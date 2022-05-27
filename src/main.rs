378109f2c85b684f6a56ebc4a69d430b25e3dfc4
Bango <fvickers0@gmail.com>
2022-05-27 11:57:38 -0700

    Bar fixed more.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 #[macro_use]
 extern crate penrose;
 
+use penrose::{
+    core::{
+        bindings::KeyEventHandler, config::Config, helpers::index_selectors, manager::WindowManager,
+    },
+    draw::{bar::dwm_bar, Draw, DrawContext, TextStyle, Color},
+    logging_error_handler,
+    xcb::{new_xcb_backed_window_manager, XcbDraw},
+    Backward, Forward, Less, More, Selector,
+};
+
 use std::{thread, time};
 
 use penrose::{
     core::{
-        bindings::KeyEventHandler,
-        config::Config,
-        helpers::index_selectors,
         data_types::{Region, WinType},
         hooks::Hook,
+        hooks::Hooks,
         xconnection::Atom,
-        manager::WindowManager,
     },
-    draw::{bar::dwm_bar, Draw, DrawContext, TextStyle},
-    logging_error_handler,
-    xcb::{new_xcb_backed_window_manager, XcbDraw},
-    Backward, Forward, Less, More, Selector,
-    Result,
 };
 
 use simplelog::{LevelFilter, SimpleLogger};
 
-
 // Replace these with your preferred terminal and program launcher
 const TERMINAL: &str = "alacritty";
 const LAUNCHER: &str = "dmenu_run";
-
 
 const HEIGHT: usize = 18;
 
@@ -41,6 +41,7 @@ const PURPLE: u32 = 0xb16286ff;
 const BLUE: u32 = 0x458588ff;
 const RED: u32 = 0xcc241dff;
 
+const FONT: &str = "ProFontIIx Nerd Font";
 
 fn main() -> penrose::Result<()> {
     // Initialise the logger (use LevelFilter::Debug to enable debug logging)
@@ -48,7 +49,6 @@ fn main() -> penrose::Result<()> {
         panic!("unable to set log level: {}", e);
     };
 
-    //simple_draw()?;
     let config = Config::default();
     let key_bindings = gen_keybindings! {
         // Program launchers
@@ -63,8 +63,8 @@ fn main() -> penrose::Result<()> {
         "M-k" => run_internal!(cycle_client, Backward);
         "M-S-j" => run_internal!(drag_client, Forward);
         "M-S-k" => run_internal!(drag_client, Backward);
-        "M-S-f" => run_internal!(toggle_client_fullscreen, &Selector::Focused);
-        "M-S-q" => run_internal!(kill_client);
+        "M-f" => run_internal!(toggle_client_fullscreen, &Selector::Focused);
+        "M-c" => run_internal!(kill_client);
 
         // workspace management
         "M-Tab" => run_internal!(toggle_workspace);
@@ -85,105 +85,32 @@ fn main() -> penrose::Result<()> {
         };
     };
 
-    let mut wm = new_xcb_backed_window_manager(config, vec![], logging_error_handler())?;
-    //bar_draw()?;
-
-    let workspaces = vec!["1", "2", "3", "4", "5", "6"];
+    
+    let height = 18;
     let style = TextStyle {
-        font: PROFONT.to_string(),
+        font: "mono".to_string(),
         point_size: 11,
-        fg: WHITE.into(),
-        bg: Some(BLACK.into()),
+        fg: Color::try_from(WHITE)?,
+        bg: Some(Color::try_from(BLACK)?),
         padding: (2.0, 2.0),
     };
-    let highlight = BLUE;
-    let empty_ws = GREY;
-    let mut bar = dwm_bar(
-        XcbDraw::new()?,
-        HEIGHT,
-        &style,
-        highlight,
-        empty_ws,
-        workspaces,
-    )?;
+    
+    let config = Config::default();
+    let hooks: Hooks<_> = vec![
+        Box::new(dwm_bar(
+            XcbDraw::new()?,
+            height,
+            &style,
+            Color::try_from(BLUE)?, // highlight
+            Color::try_from(GREY)?, // empty_ws
+            config.workspaces().clone(),
+        )?)
+    ];
+    
 
-    //let mut wm = new_xcb_backed_window_manager(Config::default(), vec![], logging_error_handler())?;
-    bar.startup(&mut wm)?;
+    let mut wm = new_xcb_backed_window_manager(config, hooks, logging_error_handler())?;
 
-    wm.grab_keys_and_run(key_bindings, map!{});
-    Ok(())
-}
+    //bar.startup(&mut wm)?; // ensure widgets are initialised correctly
 
-
-/*
-fn bar_draw() -> Result<()> {
-    let workspaces = vec!["1", "2", "3", "4", "5", "6"];
-    let style = TextStyle {
-        font: PROFONT.to_string(),
-        point_size: 11,
-        fg: WHITE.into(),
-        bg: Some(BLACK.into()),
-        padding: (2.0, 2.0),
-    };
-    let highlight = BLUE;
-    let empty_ws = GREY;
-    let mut bar = dwm_bar(
-        XcbDraw::new()?,
-        HEIGHT,
-        &style,
-        highlight,
-        empty_ws,
-        workspaces,
-    )?;
-
-    //let mut wm = new_xcb_backed_window_manager(Config::default(), vec![], logging_error_handler())?;
-    bar.startup(&mut wm)?; // ensure widgets are initialised correctly
-    /*
-    thread::sleep(time::Duration::from_millis(1000));
-    for focused in 1..6 {x
-        bar.workspace_change(&mut wm, focused - 1, focused)?;
-        bar.event_handled(&mut wm)?;
-        thread::sleep(time::Duration::from_millis(1000));
-    }*/
-
-    //thread::sleep(time::Duration::from_millis(10000));
-    Ok(())
-}*/
-
-fn simple_draw() -> Result<()> {
-    let mut drw = XcbDraw::new()?;
-    let (_, _, w, _) = drw.screen_sizes()?[0].values();
-    let id = drw.new_window(
-        WinType::InputOutput(Atom::NetWindowTypeNormal),
-        Region::new(0, 0, w, HEIGHT as u32),
-        false,
-    )?;
-    drw.register_font(PROFONT);
-    drw.register_font(SERIF);
-    drw.register_font(FIRA);
-
-    let mut ctx = drw.context_for(id)?;
-
-    ctx.color(&WHITE.into());
-    ctx.rectangle(0.0, 0.0, w as f64, HEIGHT as f64);
-    ctx.translate(1.0, 1.0);
-
-    ctx.color(&BLACK.into());
-    ctx.font(PROFONT, 12)?;
-    let (offset, _) = ctx.text("this is a simple test", 0.0, (0.0, 8.0))?;
-
-    ctx.color(&RED.into());
-    ctx.font(SERIF, 10)?;
-    ctx.translate((offset + 5.0) as f64, 0.0);
-    let (offset, _) = ctx.text("BORK BORK!", 0.0, (0.0, 0.0))?;
-
-    ctx.color(&PURPLE.into());
-    ctx.font(FIRA, 10)?;
-    ctx.translate((offset + 5.0) as f64, 0.0);
-    ctx.text("Look at all the colors!", 0.0, (0.0, 0.0))?;
-
-    drw.flush(id)?;
-    thread::sleep(time::Duration::from_millis(5000));
-    Ok(())
-
+    wm.grab_keys_and_run(key_bindings, map!{})
 }
